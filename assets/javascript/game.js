bf8d265619e371cc85e770e336f2b0e6afcf62da
Kristin Faner <kfaner@gmail.com>
2016-08-14 09:46:22 -0500

    set up html structure, char variables, some style

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+var characters = {
+	corgi: {
+		name: "Cam Corgi",
+		healthPoints: 150,
+		baseAttack: 7,
+		attackPower: 7,
+		counterAttackPower: 19,
+		idElem: "corgi",
+		activeImg: url("assets/images/charPlaceholder.png"),
+		defeatedImg: url("assets/images/charPlaceholder.png") 
+	},
+	scottie: {
+		name: "Samuel Scottie",
+		healthPoints: 100,
+		baseAttack: 9,
+		attackPower: 9,
+		counterAttackPower: 17,
+		idElem: "scottie",
+		activeImg: url("assets/images/charPlaceholder.png"),
+		defeatedImg: url("assets/images/charPlaceholder.png") 
+	},
+	akiba: {
+		name: "Alisha Akiba",
+		healthPoints: 180,
+		baseAttack: 6,
+		attackPower: 6,
+		counterAttackPower: 20,
+		idElem: "akiba",
+		activeImg: url("assets/images/charPlaceholder.png"),
+		defeatedImg: url("assets/images/charPlaceholder.png") 
+	},
+	frenchie: {
+		name: "Flora Frenchie",
+		healthPoints: 120,
+		baseAttack: 8,
+		attackPower: 8,
+		counterAttackPower: 18,
+		idElem: "frenchie",
+		activeImg: url("assets/images/charPlaceholder.png"),
+		defeatedImg: url("assets/images/charPlaceholder.png") 
+	}
+}
