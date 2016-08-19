bc748dfc81c84b70795744dceb12b1cd51df9875
Kristin Faner <kfaner@gmail.com>
2016-08-18 21:38:50 -0500

    styles, functions, debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,8 @@ var characters = {
 		attackPower: 7,
 		counterAttackPower: 19,
 		idElem: "corgi",
-		activeImg: url("assets/images/charPlaceholder.png"),
-		defeatedImg: url("assets/images/charPlaceholderDead.png") 
+		activeImg: url="/assets/images/charPlaceholder.png",
+		defeatedImg: url="assets/images/charPlaceholderDead.png" 
 	},
 	scottie: {
 		name: "Samuel Scottie",
@@ -16,8 +16,8 @@ var characters = {
 		attackPower: 9,
 		counterAttackPower: 17,
 		idElem: "scottie",
-		activeImg: url("assets/images/charPlaceholder.png"),
-		defeatedImg: url("assets/images/charPlaceholderDead.png") 
+		activeImg: url="assets/images/charPlaceholder.png",
+		defeatedImg: url="assets/images/charPlaceholderDead.png" 
 	},
 	akiba: {
 		name: "Alisha Akiba",
@@ -26,8 +26,8 @@ var characters = {
 		attackPower: 6,
 		counterAttackPower: 20,
 		idElem: "akiba",
-		activeImg: url("assets/images/charPlaceholder.png"),
-		defeatedImg: url("assets/images/charPlaceholderDead.png") 
+		activeImg: url="assets/images/charPlaceholder.png",
+		defeatedImg: url="assets/images/charPlaceholderDead.png" 
 	},
 	frenchie: {
 		name: "Flora Frenchie",
@@ -36,7 +36,52 @@ var characters = {
 		attackPower: 8,
 		counterAttackPower: 18,
 		idElem: "frenchie",
-		activeImg: url("assets/images/charPlaceholder.png"),
-		defeatedImg: url("assets/images/charPlaceholderDead.png") 
+		activeImg: url="assets/images/charPlaceholder.png",
+		defeatedImg: url="assets/images/charPlaceholderDead.png" 
 	}
 }
+var isPlayerSelected = false;
+var isFightActive = false;
+var isGameActive = true;
+var playerCharacter = "";
+var currentEnemy = "";
+
+function whatWasClicked() {
+	if ((isFightActive === false) && (isGameActive === true)) {
+		if (isPlayerSelected === false) {
+			playerCharacter = this.id;
+			isPlayerSelected = true;
+			console.log("this is the playerCharacter: " + playerCharacter);
+			$("#player-character").append($("#" + playerCharacter.toString()));
+			$("#" + playerCharacter.toString()).removeClass("default-character");
+			$(".default-character").appendTo($(".waiting-enemies"));
+		}
+		else if (playerCharacter !== this.id) {
+			currentEnemy = this.id;
+			isFightActive = true;
+			console.log("this is the currentEnemy: " + currentEnemy);
+			$("#current-enemy").append($("#" + currentEnemy.toString()));
+		} 
+	}
+
+} 
+function attack() {
+	if ((isFightActive === true) && (characters[playerCharacter].healthPoints > 0) && (characters[currentEnemy].healthPoints > 0)) {
+		console.log("Player's HP: " + playerCharacter + " " + characters[playerCharacter].healthPoints)
+		console.log("Enemy's HP: " + currentEnemy + " " + characters[currentEnemy].healthPoints)
+		characters[playerCharacter].healthPoints -= characters[currentEnemy].counterAttackPower;
+		characters[currentEnemy].healthPoints -= characters[playerCharacter].baseAttack;
+		console.log("Player's HP: " + playerCharacter + " " + characters[playerCharacter].healthPoints)
+		console.log("Enemy's HP: " + currentEnemy + " " + characters[currentEnemy].healthPoints)
+	}
+}
+function moveEnemies() {
+	// move current enemy to Arena 
+}
+
+$(".character").on("click", whatWasClicked);
+$("#attack").on("click",attack);
+
+
+
+
